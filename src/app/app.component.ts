8f2f49e9678be829e9cd09445249971cf3f023ca
Ali Saad <el.zo3by@gmail.com>
2023-03-19 16:17:48 +0200

    roject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
+import { every } from 'rxjs';
+
+type Todo = {
+  title: string;
+  complet: boolean;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lab1';
+  textFeild: string = '';
+  data: any = localStorage.getItem('todos');
+  todos: Todo[] = JSON.parse(this.data) || [];
+  theme: number = 1;
+
+  addTodo(): void {
+    if (this.textFeild)
+      this.todos.push({ title: this.textFeild, complet: false });
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  removeTodo(index: number): void {
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+  combleteTodo(index: number): void {
+    this.todos[index].complet = !this.todos[index].complet;
+  }
 }
